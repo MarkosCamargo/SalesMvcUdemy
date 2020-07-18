89bf0be32c3a30762289c7bbf0805e0b45fd5702
Marcos Camargo <markos_camargo@hotmail.com>
2020-07-18 13:04:18 -0300

    Clear Imports.

--- SalesWebMvc/Data/SeedingService.cs
+++ SalesWebMvc/Data/SeedingService.cs
@@ -1,9 +1,7 @@
 ﻿using SalesWebMvc.Models;
 using SalesWebMvc.Models.Enums;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace SalesWebMvc.Data
 {
--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -1,9 +1,7 @@
 ﻿using SalesWebMvc.Data;
 using SalesWebMvc.Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace SalesWebMvc.Services
 {
