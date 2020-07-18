ee7c35d4186e62aa184a15bb5b05696c9788d0ae
Marcos Camargo <markos_camargo@hotmail.com>
2020-07-18 12:07:19 -0300

    SellerService and basic FindAll and layout definition.

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
 {
     public class SellersController : Controller
     {
+
+        /**
+        * <summary>Serviço Seller(ModelBO)</summary>
+        */
+        private readonly SellerService _sellersService;
+
+        /// <summary>
+        /// Construtor com injeção de dependencia, definida no Startup.cs
+        /// </summary>
+        public SellersController(SellerService sellersService)
+        {
+            _sellersService = sellersService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var list = _sellersService.FindAll();
+            return View(list);
         }
+
     }
 }
---
+++ SalesWebMvc/Services/SellerService.cs
@@ -0,0 +1,31 @@
+﻿using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMvcContext _context;
+
+        /// <summary>
+        /// Construtor com injeção de dependencia, definida no Startup.cs
+        /// </summary>
+        public SellerService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Retorna todos os Sellers do banco de dados
+        /// </summary>
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+
+    }
+}
