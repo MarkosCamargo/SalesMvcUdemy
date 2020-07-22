6f993bb962071018a49fe1d1b770ddf92d2a0d29
Marcos Camargo <markos_camargo@hotmail.com>
2020-07-22 19:51:12 -0300

    Implementing simple search.

--- SalesWebMvc/Controllers/SalesRecordsController.cs
+++ SalesWebMvc/Controllers/SalesRecordsController.cs
@@ -3,18 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
 {
     public class SalesRecordsController : Controller
     {
+        /**
+        * <summary>Serviço SalesRecord(ModelBO)</summary>
+        */
+        private readonly SalesRecordService _salesRecordService;
+ 
+        /// <summary>
+        /// Construtor com injeção de dependencia, definida no Startup.cs
+        /// </summary>
+        public SalesRecordsController(SalesRecordService salesRecordService)
+        {
+            _salesRecordService = salesRecordService;
+        }
         public IActionResult Index()
         {
             return View();
         }
-        public IActionResult SimpleSearch()
+        public async Task<IActionResult> SimpleSearch(DateTime? minDate, DateTime? maxDate)
         {
-            return View();
+            if (!minDate.HasValue) 
+            {
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+            if (!maxDate.HasValue)
+            {
+                maxDate = DateTime.Now;
+            }
+            ViewData["minDate"] = getDateFormat(minDate.Value);
+            ViewData["maxDate"] = getDateFormat(maxDate.Value);
+            var result = await _salesRecordService.FindByDateAsync(minDate, maxDate);
+            return View(result);
+        }
+
+        private string getDateFormat(DateTime date, string format = "yyyy-MM-dd") 
+        {
+           return date.ToString(format);
         }
         public IActionResult GroupingSearch()
         {
---
+++ SalesWebMvc/Services/SalesRecordService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Services
+{
+    public class SalesRecordService
+    {
+        private readonly SalesWebMvcContext _context;
+
+        /// <summary>
+        /// Construtor com injeção de dependencia, definida no Startup.cs
+        /// </summary>
+        public SalesRecordService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate) 
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue) 
+            {
+                result = result.Where(sales => sales.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(sales => sales.Date <= maxDate.Value);
+            }
+            return await result.
+                Include(sales => sales.Seller).
+                Include(sales => sales.Seller.Department).
+                OrderByDescending(sales => sales.Date)
+                .ToListAsync();
+        }
+
+    }
+}
